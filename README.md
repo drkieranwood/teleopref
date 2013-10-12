60019a0d40203ff801b351fceab31e9cb0138425
Kiran Wood <kieranwood85@yahoo.co.uk>
2013-10-12 20:06:28 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+teleopref
+========
+
+A node to monitor keyboard inputs and update a reference coordinate topic.
---
+++ include/teleopref/teleopref.h
@@ -0,0 +1,9 @@
+#ifndef _TELEOPREF_SYSTEM_H
+#define _TELEOPREF_SYSTEM_H
+
+
+//Classes and definitions for the teleopref node
+
+
+#endif
+//eof
